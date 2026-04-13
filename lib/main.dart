0f809d298737b5742ab1b9d881dd57f763a61ac5
Saiyam506 <saiyam@medidocs.in>
2026-04-13 20:23:16 +0530

    blabla

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: Padding(
+          padding: EdgeInsetsGeometry.only(left: 30),
+          child: Image.asset('assets/company/logo_with_text.png', height: 100),
+        ),
+      ),
+    );
+  }
+}
